c88b09660a396aa56db1d8b7dfd6b7dea453374e
Jimmy <Jimmy@fsdfs.com>
2024-10-05 16:05:32 +0800

    fix ESM Bundler warn

--- vue.config.js
+++ vue.config.js
@@ -4,14 +4,14 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/inventory-front-end-vue/'
     : '/',
-  // configureWebpack: {
-  //   plugins: [
-  //     new webpack.DefinePlugin({
-  //       __VUE_OPTIONS_API__: JSON.stringify(true), // 如果你使用 Vue Options API，設置為 true
-  //       __VUE_PROD_DEVTOOLS__: JSON.stringify(false), // 生產環境中禁用 Vue Devtools
-  //       __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false) // 禁用 hydration mismatch 詳細信息
-  //     })
-  //   ]
-  // }
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_OPTIONS_API__: JSON.stringify(true), // 如果你使用 Vue Options API，設置為 true
+        __VUE_PROD_DEVTOOLS__: JSON.stringify(false), // 生產環境中禁用 Vue Devtools
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false) // 禁用 hydration mismatch 詳細信息
+      })
+    ]
+  }
 
 }
