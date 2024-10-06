ef811da8522293e4ba51d630daed5fea93ac1e47
Jimmy <Jimmy@fsdfs.com>
2024-10-07 01:06:45 +0800

    deploy

--- assets/production_process_items-D3N6xVix.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./index-DnqVUwQ5.js";const e={getAll(){return r.get("/partner_factories")}},o={getAll(){return r.get("/production_process_items")}};export{o as P,e as p};
---
+++ assets/production_process_items-Dlxex6Cd.js
@@ -0,0 +1,1 @@
+import{a as r}from"./index-Dd-8Ru_X.js";const e={getAll(){return r.get("/partner_factories")}},o={getAll(){return r.get("/production_process_items")}};export{o as P,e as p};
