7af08d82460eab8be834d0941a0173230640623a
Jimmy <Jimmy@fsdfs.com>
2024-10-10 04:44:52 +0800

    add tailwind class name

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   prefix: 't-',
-  content: ["./index.html',","./src/**/*.{html,js,vue,ts}"],
+  mode: 'jit',
+  content: ["./index.html',","./src/**/*.{html,js,vue,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
