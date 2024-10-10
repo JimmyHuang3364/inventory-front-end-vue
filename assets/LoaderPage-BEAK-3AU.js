af77e893e8a5d1312cce278fa0a17f634b8a3efe
Jimmy <Jimmy@fsdfs.com>
2024-10-10 16:15:48 +0800

    deploy

--- assets/LoaderPage-BEAK-3AU.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as n,b as t,o}from"./index-D4_0BwGz.js";const a={},r={class:"loader-center m-0 p-0 text-white d-flex justify-content-center align-items-center"};function c(d,e,l,i,p,_){return o(),n("div",r,e[0]||(e[0]=[t("div",{class:"loader-ring"},null,-1),t("span",null,"loading...",-1)]))}const u=s(a,[["render",c],["__scopeId","data-v-01b534d5"]]);export{u as default};
