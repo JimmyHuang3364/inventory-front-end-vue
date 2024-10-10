d4cfd7cbabf9fa4543d3a5145e9d985c53f828fd
Jimmy <Jimmy@fsdfs.com>
2024-10-11 01:09:22 +0800

    add PersonalPractiseGeneral & PractiseSwiper componerts
    補上缺少的

---
+++ src/views/personalpractisepages/PersonalPractiseGeneral.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <PractiseSwiper />
+  </div>
+</template>
+<script setup lang="ts">
+  import PractiseSwiper from './components/PractiseSwiper.vue';
+</script>
+<style lang="">
+  
+</style>
